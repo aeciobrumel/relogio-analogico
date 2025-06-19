ae073067995dc07134b3990c07e32e7775db13d1
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2025-06-19 18:09:56 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
-let digitalElement = document.querySelector("digital");
-let sElement = document.querySelector("p_s");
-let mElement;
+let digitalElement = document.querySelector(".digital");
+let sElement = document.querySelector(".p_s");
+let mElement = document.querySelector(".p_m");
+let hElement = document.querySelector(".p_h");
+
+function updateClock() {
+  let now = new Date();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+
+  digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(
+    second
+  )}`;
+
+  let ps = (360 / 60) * second - 90;
+  let ms = (360 / 60) * minute - 90;
+  let hs = (360 / 12) * hour - 90;
+
+  sElement.style.transform = `rotate(${ps}deg)`;
+  mElement.style.transform = `rotate(${ms}deg)`;
+  hElement.style.transform = `rotate(${hs}deg)`;
+}
+
+function fixZero(date) {
+  return date < 10 ? `0${date}` : date;
+}
+/* 
+//fix zero, function
+ com if inline 
+
+elemento .styletrnasforn = rotate valor deeg
+
+
+
+ criculo 360g
+ /60
+
+ cada seg 6gr
+
+ menos o 0 
+ fixo em -90 no inicio
+
+*/
+setInterval(updateClock, 1000);
+updateClock();
